7e243c084b9bd83ef66245aabfcac7a9d4d0cace
Manvinder <manvindersingh086.com>
2023-04-08 11:49:23 +1000

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "../components/Header.module.css";
+
+const header = (props : any) => {
+    return (
+        <>
+            <h1 className={classes.htag}>{props.header}</h1>
+        </>
+    )
+}
+
+export default header;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import {createApi} from 'unsplash-js';
+import classes from './SearchBar.module.css'
+
+const SearchBar = () => {
+
+    const API_KEY : string = process.env.REACT_APP_API_KEY!;
+    const [searchName, setSearchName] = useState("");
+    const [searchResult, setSearchResult] = useState([] as any)
+    const unsplash = createApi({
+        accessKey: API_KEY
+    })
+
+    const searchNameHandler = (e : any) => {
+        setSearchName(e.target.value)
+    }
+
+    const formSubmitHandler = async (event : any) => {
+        event.preventDefault();
+        const response = await unsplash.search.getPhotos({
+            query: searchName,
+            page: 1,
+            perPage: 20,
+            color: 'green',
+            orientation: 'portrait',
+          });
+          console.log(response)
+          setSearchResult(response.response?.results);
+        
+        }
+    
+
+    return (
+            <>
+                <form onSubmit={formSubmitHandler}>
+                    <input type="text" className={classes.textField} name="query"  placeholder="Try any logical name" onChange={searchNameHandler} />
+                    <input type="submit" className={classes.buttonField} value="Search" />
+                </form>
+                <div className={classes.imgtag}>
+                {searchResult.map((pic : any ) =>
+          <div key={pic.id}>
+            <img
+              alt={pic.alt_description}
+              src={pic.urls.full}
+              width="80%"
+              height="80%"
+            ></img>
+          </div>)}
+
+                </div>
+            </>
+        );
+}
+
+export default SearchBar;
